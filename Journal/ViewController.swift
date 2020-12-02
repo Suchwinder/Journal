f0367123fd9a42c1517dd6f9df2549231bba8867
Suchwinder Singh <windersingh80@yahoo.com>
2020-12-02 15:45:41 -0500

    First Commit
    Set up first screen textfields and some button functionality

--- Journal/ViewController.swift
+++ Journal/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // Helper function to ensure fields arent empty when trying to use app
+    func checkFields() -> Bool {
+        if username.text == "" || password.text == "" {
+            let alert = UIAlertController(title: "Invalid Entry",
+                                            message: "Please be sure all fields are filled",
+                                            preferredStyle: .alert)
+            let action = UIAlertAction(title: "Confirm", style: .default, handler: nil)
+            
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+            return false;
+        }
+        return true;
+    }
+    
+    @IBAction func handleSignIn () {
+        if checkFields() {
+            print("Contents of the username: \(username.text!)")
+            
+            print("Contents of the password: \(password.text!)")
+        }
+    }
+    @IBAction func handleSignUp () {
+        if checkFields() {
+            print("Contents of the username: \(username.text!)")
+            
+            print("Contents of the password: \(password.text!)")
+        }
+    }
 }
 

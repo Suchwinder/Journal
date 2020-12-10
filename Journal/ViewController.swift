5291e0a4cbfb2e1a436be9807410919f29a8d5a0
Suchwinder Singh <windersingh80@yahoo.com>
2020-12-10 00:28:30 -0500

    user sign in and log out successful

--- Journal/ViewController.swift
+++ Journal/ViewController.swift
@@ -46,28 +46,116 @@ class ViewController: UIViewController {
         return true
     }
     
+    // alert user doesn't exist
+    func showAlert () {
+        let alert = UIAlertController(title: "Invalid User",
+                                      message: "User Doesn't Exist",
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title: "Confirm", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
     // This is to satisfy the audio through AV
     // https://developer.apple.com/documentation/avfoundation/avspeechsynthesizer
     @IBAction func handleSignIn () {
         if checkFields() {
+            // simple session object with limited default behavior
+            let session = URLSession.shared
+            // provides the resource that we will be making get request to
+            let url = URL(string: "https://ios-journal.herokuapp.com/api/user/login")!
+            
+            // make it post request to pass data as parameters
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            // set dictionary with contents we will pass
+            let json = [
+                "username": username.text,
+                "password": password.text
+            ]
+            
+            // convert to json
+            let jsonData = try! JSONSerialization.data(withJSONObject: json, options: [])
+            
+            // set up closure to execute functions and store its results
+            let task = session.uploadTask(with: request, from: jsonData) {data, response, error in
+                if let httpResponse = response as? HTTPURLResponse,
+                   (200...299).contains(httpResponse.statusCode) {
+                    DispatchQueue.main.async {
+                        if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "JournalViewController") as? UINavigationController {
+                            vc.modalPresentationStyle = .fullScreen
+                            self.present(vc, animated: true, completion: nil)
+                        }
+                    }
+                }
+                else {
+                    DispatchQueue.main.async {
+                        self.showAlert()
+                    }
+                    return
+                }
+            }
+            task.resume() // executing request
+            
+            // https://stackoverflow.com/questions/39929592/how-to-push-and-present-to-uiviewcontroller-programmatically-without-segue-in-io
+            
             username.text = ""
             password.text = ""
-            
-//            // https://nshipster.com/avspeechsynthesizer/
-//            let speechSynthesizer = AVSpeechSynthesizer() // Object that will take in a speechUtterance object to speak
-//            let speechUtterance: AVSpeechUtterance = AVSpeechUtterance(string: "Welcome to your Journal") // Speech Utterance object that had the phrase to speak
-//            speechUtterance.rate = AVSpeechUtteranceMaximumSpeechRate / 1.8 // Can adjust speed, larger division number the slower it is
-//            speechUtterance.voice = AVSpeechSynthesisVoice(language: "en-US") // Ensure it is in US English
-//            // causes an warning
-//            speechSynthesizer.speak(speechUtterance) // pass object to execute speech
-//            dismiss(animated: true, completion: nil) // transition into next screen
         }
     }
     
     @IBAction func handleSignUp () {
         if checkFields() {
-            username.text = ""
-            password.text = ""
+            
+            // simple session object with limited default behavior
+            let session = URLSession.shared
+            // provides the resource that we will be making get request to
+            let url = URL(string: "https://ios-journal.herokuapp.com/api/user/signup")!
+            
+            // make it post request to pass data as parameters
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            // set dictionary with contents we will pass
+            let json = [
+                "username": username.text,
+                "password": password.text
+            ]
+            
+            // convert to json
+            let jsonData = try! JSONSerialization.data(withJSONObject: json, options: [])
+            
+            // set up closure to execute functions and store its results
+            let task = session.uploadTask(with: request, from: jsonData) {data, response, error in
+                if let httpResponse = response as? HTTPURLResponse,
+                   (200...299).contains(httpResponse.statusCode) {
+                    DispatchQueue.main.async {
+                        self.handleSignIn()
+                    }
+                }
+                else {
+                    DispatchQueue.main.async {
+                        self.username.text = "";
+                        self.password.text = "";
+                        let alert = UIAlertController(title: "Invalid Entry",
+                                                      message: "User Already Exists",
+                                                      preferredStyle: .alert)
+                        let action = UIAlertAction(title: "Confirm", style: .default, handler: nil)
+                        
+                        alert.addAction(action)
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                    return
+                }
+            }
+            task.resume() // executing request
+            
         }
     }
 }
